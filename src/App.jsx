92f9e94ee9c60f0d7155e7e96999390d5a6fc463
Nino <135982201+ninoozz@users.noreply.github.com>
2025-06-06 14:07:44 -0300

    Inicio-React-add/remove-tasks

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/tasks";
+import "./index.css";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudando codando e pa",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar desenho",
+      description: "Estudando desenhando e pa",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function deleteTask(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+  function onSubmitAddTask(title, description) {
+    const newTask = {
+      id: uuidv4(),
+      title: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex flex-col justify-center p-6 items-center ">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-white font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask onSubmitAddTask={onSubmitAddTask} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          deleteTask={deleteTask}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,31 @@
+import { ChevronRightIcon, Trash2Icon } from "lucide-react";
+
+function Tasks(props) {
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full text-left ${
+              task.isCompleted && `line-through`
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.deleteTask(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <Trash2Icon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
